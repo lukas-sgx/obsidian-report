36c8de78519ee89cd8aa47dc3caa769033aa24f7
lukas-sgx <lukas.soigneux@epitech.eu>
2026-04-12 01:57:36 +0200

    style: add space for visibility

--- turbine_explode/auto_runner.py
+++ turbine_explode/auto_runner.py
@@ -5,9 +5,12 @@ import os
 
 env = os.environ.copy()
 env['LD_LIBRARY_PATH'] = './runner/external/'
-p = process(['./runner/run.sh'], env=env)
-p.recv(numb=100, timeout=1.0)
-p.sendline(b'turbine_temperature')
-p.recv(timeout=1.0)
-p.sendline(b'2147483646')
-print(p.clean().decode(errors='ignore'))
+
+pid = process(['./runner/run.sh'], env=env)
+pid.recv(numb=100, timeout=1.0)
+
+pid.sendline(b'turbine_temperature')
+pid.recv(timeout=1.0)
+pid.sendline(b'2147483646')
+
+print(pid.clean().decode(errors='ignore'))

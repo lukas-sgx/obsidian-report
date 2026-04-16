397dd15393f19efc00c30a93c216333006ea8cf3
lukas-sgx <lukas.soigneux@epitech.eu>
2026-04-16 19:51:25 +0200

    feat: implement run_diagnostic

---
+++ run_diagnostic/auto_runner.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+from pwn import *
+import os
+import warnings
+
+def main():
+    warnings.filterwarnings("ignore", category=BytesWarning)
+    context.log_level = 'error'
+
+    env = os.environ.copy()
+    env['LD_LIBRARY_PATH'] = './runner/external/'
+
+    pid = process(['./runner/run.sh'], env=env)
+    pid.recv(numb=100, timeout=1.0)
+    pid.sendline(b'run_diagnostic')
+    pid.recv(timeout=1.0)
+    pid.sendline(b'debug')
+    line = pid.recvlines(timeout=1.0)
+
+    print(text.red("[+]") + " Vuln Weak Encryption - run_diagnostic:")
+    print(line[1].decode()[19:])
+    print()
+
+
+if __name__ == "__main__":
+    main()
